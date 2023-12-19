8fbd6a4a6bf310d39b081ad5f3a455e9f8858490
Sanjana Bafana <sanjanabafna093@gmail.com>
2023-12-19 22:19:21 +0530

    matplot library remove from code

--- deploy.py
+++ deploy.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
